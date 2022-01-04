f53ea678aea9aaf4d66a985bf4923d38511e24df
Ethan Chandler <86173119+IzzTinkr@users.noreply.github.com>
2022-01-04 16:33:30 -0600

    gallery items done along with contact form

--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -1,103 +1,47 @@
-// import React from "react";
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-// function Nav() {
-//   const categories = [
-//     {
-//       name: "commercial",
-//       description:
-//         "Photos of grocery stores, food trucks, and other commercial projects",
-//     },
-//     { name: "portraits", description: "Portraits of people in my life" },
-//     { name: "food", description: "Delicious delicacies" },
-//     {
-//       name: "landscape",
-//       description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-//     },
-//   ];
+function Nav(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    contactSelected,
+    currentCategory,
+    setContactSelected,
+  } = props;
 
-//   function categorySelected(name) {
-//     console.log(`${name} clicked`);
-//   }
-
-//   return (
-//     <header>
-//       <h2>
-//         <a href="/">
-//           <span role="img" aria-label="camera">
-//             {" "}
-//             📸
-//           </span>{" "}
-//           Oh Snap!
-//         </a>
-//       </h2>
-//       <nav>
-//         <ul className="flex-row">
-//           <li className="mx-2">
-//             <a href="#about">About me</a>
-//           </li>
-//           <li>
-//             <span>Contact</span>
-//           </li>
-//           {categories.map((category) => (
-//             <li className="mx-1" key={category.name}>
-//               <span onClick={{ categorySelected }}>{category.name}</span>
-//             </li>
-//           ))}
-//         </ul>
-//       </nav>
-//     </header>
-//   );
-// }
-
-// export default Nav;
-import React from "react";
-import { capitalizeFirstLetter } from "../../utils/helpers";
-
-function Nav() {
-  const categories = [
-    {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-  ];
-
-  const handleClick = () => {
-    console.log("click handled");
-  };
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
-    <header data-testid="header" className="flex-row px-1">
+    <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera">
-            {" "}
-            📸
-          </span>{" "}
-          Oh Snap!
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => handleClick()}>
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
-          <li className={"mx-2"}>
-            <span onClick={() => handleClick()}>Contact</span>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
-            <li className="mx-1" key={category.name}>
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && !contactSelected && 'navActive'
+                }`}
+              key={category.name}
+            >
               <span
                 onClick={() => {
-                  handleClick();
+                  setCurrentCategory(category);
+                  setContactSelected(false);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
